922d9a8322bf52224ccd2ed462e5b1bcf28f54e5
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-06-28 11:32:35 -0400

    add build folder

---
+++ build/precache-manifest.cc67e8536a301a8210cd7bacee771923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8389497d271b4c11e67c9f5a7551672c",
+    "url": "fearmichael.github.io/react-portfolio/index.html"
+  },
+  {
+    "revision": "dbbcf8491237a38cff14",
+    "url": "fearmichael.github.io/react-portfolio/static/css/main.e9bb1ebb.chunk.css"
+  },
+  {
+    "revision": "bf1ad7ab12106d9824dd",
+    "url": "fearmichael.github.io/react-portfolio/static/js/2.46ce45ad.chunk.js"
+  },
+  {
+    "revision": "dbbcf8491237a38cff14",
+    "url": "fearmichael.github.io/react-portfolio/static/js/main.949128eb.chunk.js"
+  },
+  {
+    "revision": "69757d772635274f67c4",
+    "url": "fearmichael.github.io/react-portfolio/static/js/runtime~main.bc9be027.js"
+  }
+]);
