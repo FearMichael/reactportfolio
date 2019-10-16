6417ff59bfaee8c695150cc392237bd4d9d78c30
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-10-16 14:58:59 -0400

    updating descriptions

--- src/portfolioInfo.js
+++ src/portfolioInfo.js
@@ -2,50 +2,50 @@ const portfolioInfo = [
     {
         name: "Bench Strength",
         picture: "./images/benchstrength.png",
-        description: "The app that provides Team Leaders the ability to assess employee skills and interests to build smarter, more invested teams.",
-        tools: ["Angular 7","Typescript","Bootstrap", "Sequelize", "Auth0","AWS"],
+        description: "A group curated app that provides Team Leaders the ability to assess employee skills and interests to build smarter, more invested teams. My primary responsibilities revolved around building front end components as well as integrating Github authentication via Auth0.",
+        tools: ["Angular 7", "Typescript", "Bootstrap", "Sequelize", "Auth0", "AWS", "Express API Server"],
         deployedLink: "http://ec2-3-80-218-219.compute-1.amazonaws.com:3000/",
-        
+
     },
     {
         name: "Daily Dash",
         picture: "./images/dailydash.png",
-        description: "Ever been annoyed at needing to go several different apps/websites to get some basic info? DailyDash consolidates all of that information into a personalized experience for you.",
-        tools: ["Sequelize","Heroku", "Materialize", "Node.js","Passportjs"],
+        description: "Ever been annoyed at needing to go to several different apps or websites to retrieve daily information? DailyDash, a team built application, consolidates all of that information into a personalized experience for you. I was responsible for authentication using Passport with a Google Provider along with the API server and data structures. I also resolved time displaying properly on the front end.",
+        tools: ["Sequelize", "Heroku", "Materialize", "Node.js", "Passportjs"],
         deployedLink: "http://dailydashboardproject2.herokuapp.com/",
-        
+
     },
     {
         name: "A Night In",
         picture: "./images/aNightIn.jpg",
-        description: "This app is designed to provide an easy user experience for finding a movie to watch and food to eat for entertainment.",
-        tools: ["Movie and Food API's","Github", "Materialize", "Firebase"],
+        description: "This service, developed by a team that enjoys various types of entertainment, is designed to provide an easy user experience for finding a movie to watch alongside food to eat. I researched and integrated third-party API’s for data. I also connected the application to Firebase in order to store and display recent search results.",
+        tools: ["Movie and Food API's", "Github", "Materialize", "Firebase"],
         deployedLink: "https://anightout-1548896687179.firebaseapp.com/",
-        
+
     },
     {
         name: "Giftastic",
         picture: "./images/gifTastic.png",
         description: "This fun web app will provide the user with 10 gifs as well as creating a button for easy re-use.",
-        tools: ["Giphy API","Github", "Materialize","jQuery"],
+        tools: ["Giphy API", "Github", "Materialize", "jQuery"],
         deployedLink: "https://fearmichael.github.io/giftastic/",
-        
+
     },
     {
         name: "Train Tracker",
         picture: "./images/trainTracker.png",
         description: "This is a prototype for tracking train schedules. This utilizes a NoSQL database (Firebase) along with authentication.",
-        tools: ["Firebase","Moment JS", "Materialize", "User input validation"],
+        tools: ["Firebase", "Moment JS", "Materialize", "User input validation"],
         deployedLink: "https://traintracker-e3faf.firebaseapp.com/",
-        
+
     },
     {
         name: "Word Guess Game",
         picture: "./images/wordGuess.png",
         description: "A fun photography themed word guess game.",
-        tools: ["Input validation","Github Version Control","Bootstrap","jQuery"],
+        tools: ["Input validation", "Github Version Control", "Bootstrap", "jQuery"],
         deployedLink: "https://fearmichael.github.io/word-guess-game/",
-        
+
     },
     // {
     //     name: "",
@@ -53,7 +53,7 @@ const portfolioInfo = [
     //     description: "",
     //     tools: "",
     //     deployedLink: "",
-        
+
     // },
 ]
 export default portfolioInfo;
